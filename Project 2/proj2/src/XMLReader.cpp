9358311ec073f1a96973af6a1cf622910853c80e
Jerry Au <hoikanau123@gmail.com>
2025-02-17 04:29:42 +0000

    push xml

--- Project 2/proj2/src/XMLReader.cpp
+++ Project 2/proj2/src/XMLReader.cpp
@@ -1,30 +1,108 @@
 #include "XMLReader.h"
-#include <iostream>
+#include "DataSource.h"
+#include "XMLEntity.h"
+#include <expat.h>
+#include <memory>
 #include <string>
 #include <vector>
-#include <expat.h>
 
-using namespace std;
+// PIMPL structure for CXMLReader
+struct CXMLReader::SImplementation {
+    std::shared_ptr<CDataSource> source;
+    std::string xmlData;              // holds the complete XML text
+    std::vector<SXMLEntity> entities;   // parsed XML entities
+    size_t currentIndex;              // next entity index to return
+    XML_Parser parser;                // Expat parser instance
 
-CXMLReader::CXMLReader(shared_ptr<CDataSource> src){
+    SImplementation(std::shared_ptr<CDataSource> src)
+        : source(src), currentIndex(0)
+    {
+        // Read the entire XML input from the data source.
+        char ch;
+        while (source->Get(ch)) {
+            xmlData.push_back(ch);
+        }
 
+        // Create an Expat parser.
+        parser = XML_ParserCreate(nullptr);
+        XML_SetUserData(parser, this);
+        XML_SetElementHandler(parser, StartElementHandler, EndElementHandler);
+        XML_SetCharacterDataHandler(parser, CharacterDataHandler);
+
+        // Parse the XML data.
+        if (XML_Parse(parser, xmlData.c_str(), static_cast<int>(xmlData.size()), XML_TRUE)
+            == XML_STATUS_ERROR)
+        {
+            // If a parsing error occurs, clear the entities.
+            entities.clear();
+        }
+        XML_ParserFree(parser);
+    }
+
+    // Callback for start elements.
+    static void XMLCALL StartElementHandler(void *userData,const XML_Char *name,const XML_Char **atts)
+    {
+        SImplementation* impl = static_cast<SImplementation*>(userData);
+        SXMLEntity entity;
+        entity.DType = SXMLEntity::EType::StartElement;
+        entity.DNameData = name;
+        // Process attributes: atts is a null-terminated array of name/value pairs.
+        if (atts) {
+            for (int i = 0; atts[i]; i += 2) {
+                entity.DAttributes.push_back(std::make_pair(atts[i], atts[i+1]));
+            }
+        }
+        impl->entities.push_back(entity);
+    }
+
+    // Callback for end elements.
+    static void XMLCALL EndElementHandler(void *userData,
+                                          const XML_Char *name)
+    {
+        SImplementation* impl = static_cast<SImplementation*>(userData);
+        SXMLEntity entity;
+        entity.DType = SXMLEntity::EType::EndElement;
+        entity.DNameData = name;
+        impl->entities.push_back(entity);
+    }
+
+    // Callback for character data.
+    static void XMLCALL CharacterDataHandler(void *userData, const XML_Char *s,int len)
+    {
+        SImplementation* impl = static_cast<SImplementation*>(userData);
+        std::string data(s, len);
+        // Create an entity for the character data.
+        SXMLEntity entity;
+        entity.DType = SXMLEntity::EType::CharData;
+        entity.DNameData = data;
+        impl->entities.push_back(entity);
+    }
+};
+
+//
+// CXMLReader Member Functions
+//
+
+CXMLReader::CXMLReader(std::shared_ptr<CDataSource> src)
+    : DImplementation(std::make_unique<SImplementation>(src))
+{
 }
 
-CXMLReader::~CXMLReader(){
+CXMLReader::~CXMLReader() = default;
 
+// End() returns true if all entities have been returned.
+bool CXMLReader::End() const {
+    return DImplementation->currentIndex >= DImplementation->entities.size();
 }
 
-bool CXMLReader::End() const{
+// ReadEntity returns the next entity (skipping character data if skipcdata==true)
+bool CXMLReader::ReadEntity(SXMLEntity &entity, bool skipcdata) {
+    while (DImplementation->currentIndex < DImplementation->entities.size()) {
+        entity = DImplementation->entities[DImplementation->currentIndex];
+        DImplementation->currentIndex++;
+        if (skipcdata && entity.DType == SXMLEntity::EType::CharData)
+            continue;
+        return true;
+    }
     return false;
 }
-
-bool CXMLReader::ReadEntity(SXMLEntity &entity, bool skipcdata){
-    return false;
-}
-
-
-        // CXMLReader(std::shared_ptr< CDataSource > src);
-        // ~CXMLReader();
-        
-        // bool End() const;
-        // bool ReadEntity(SXMLEntity &entity, bool skipcdata = false);
---
+++ Project 2/proj2/src/XMLWriter.cpp
@@ -0,0 +1,112 @@
+#include "XMLWriter.h"
+#include "DataSink.h"
+#include "XMLEntity.h"
+#include <memory>
+#include <string>
+#include <vector>
+
+// PIMPL structure for CXMLWriter
+struct CXMLWriter::SImplementation {
+    std::shared_ptr<CDataSink> sink;
+    std::vector<std::string> openElements; // Stack of currently open element names
+
+    SImplementation(std::shared_ptr<CDataSink> s)
+        : sink(s)
+    { }
+
+    // Helper function to write a string to the sink.
+    bool WriteStr(const std::string &str) {
+        std::vector<char> buf(str.begin(), str.end());
+        return sink->Write(buf);
+    }
+
+    // Helper function to escape XML special characters.
+    std::string Escape(const std::string &str) {
+        std::string out;
+        for (char c : str) {
+            switch (c) {
+                case '&':  out.append("&amp;"); break;
+                case '<':  out.append("&lt;"); break;
+                case '>':  out.append("&gt;"); break;
+                case '\"': out.append("&quot;"); break;
+                case '\'': out.append("&apos;"); break;
+                default:   out.push_back(c); break;
+            }
+        }
+        return out;
+    }
+};
+
+//
+// CXMLWriter Member Functions
+//
+
+CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink)
+    : DImplementation(std::make_unique<SImplementation>(sink))
+{
+}
+
+CXMLWriter::~CXMLWriter() = default;
+
+// WriteEntity writes out the supplied XML entity.
+bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
+    if (!DImplementation->sink)
+        return false;
+
+    std::string output;
+    switch(entity.DType) {
+        case SXMLEntity::EType::StartElement:
+            // Write a start tag with attributes.
+            output = "<" + entity.DNameData;
+            for (const auto &attr : entity.DAttributes) {
+                output += " " + attr.first + "=\"" + DImplementation->Escape(attr.second) + "\"";
+            }
+            output += ">";
+            if (!DImplementation->WriteStr(output))
+                return false;
+            DImplementation->openElements.push_back(entity.DNameData);
+            break;
+        case SXMLEntity::EType::EndElement:
+            // If matching an open element, pop it.
+            if (!DImplementation->openElements.empty() &&
+                DImplementation->openElements.back() == entity.DNameData)
+            {
+                DImplementation->openElements.pop_back();
+            }
+            output = "</" + entity.DNameData + ">";
+            if (!DImplementation->WriteStr(output))
+                return false;
+            break;
+        case SXMLEntity::EType::CompleteElement:
+            // Write a self-closing tag.
+            output = "<" + entity.DNameData;
+            for (const auto &attr : entity.DAttributes) {
+                output += " " + attr.first + "=\"" + DImplementation->Escape(attr.second) + "\"";
+            }
+            output += "/>";
+            if (!DImplementation->WriteStr(output))
+                return false;
+            break;
+        case SXMLEntity::EType::CharData:
+            // Write escaped character data.
+            output = DImplementation->Escape(entity.DNameData);
+            if (!DImplementation->WriteStr(output))
+                return false;
+            break;
+        default:
+            return false;
+    }
+    return true;
+}
+
+// Flush outputs end tags for any remaining open elements.
+bool CXMLWriter::Flush() {
+    bool success = true;
+    while (!DImplementation->openElements.empty()) {
+        std::string tag = DImplementation->openElements.back();
+        DImplementation->openElements.pop_back();
+        std::string output = "</" + tag + ">";
+        success = success && DImplementation->WriteStr(output);
+    }
+    return success;
+}
